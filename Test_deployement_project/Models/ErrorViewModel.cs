c8be1e7e4bce2f00c5b81eb1228e13da766af5db
hoa-minzy <haveaniceday6174@gmail.com>
2025-12-18 07:42:15 +0700

    Add project files.

---
+++ Test_deployement_project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_deployement_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
